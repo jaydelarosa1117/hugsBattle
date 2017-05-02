53478130896d34acdd5d58aa0904d3b441f80a59
Marina Chong <marinachong96@gmail.com>
2017-05-01 21:10:49 -0600

    Update Main.hs

--- Main.hs
+++ Main.hs
@@ -5,18 +5,22 @@ import System.Random
 import System.IO.Unsafe
 import Board
 
-placeShips :: [[Int]] -> [[Int]] -> [[Int]]
+rand :: Bool -> Int
+rand x = unsafePerformIO randomIO
+
+generateRandom :: Int -> Int
+generateRandom n = ((mod (rand True) n))
+
 placeShips [] board = board
-placeShips (h:t) board = do
-							x <- randomRIO(1,10)
-						    y <- randomRIO(1,10)
-						    dir <- randomRIO(1,2)
-							if isShipPlaceable h x y (dir==1) board 
-							then placeShips t (placeShip h x y (dir==1) board))
-					    	else placeShips (h:t) board
-							
+placeShips (h:t) board = do 
+						 if isShipPlaceable h x y (dir == 1) board
+						 then placeShips t (placeShip h x y (dir==1) board)
+						 else placeShips (h:t) board where
+						 {x = generateRandom 11; y = generateRandom 11; dir = generateRandom 2}
+
+
 getXY board =	do
-       putStrLn "Enter a positive x value?"
+       putStrLn "Enter a positive x value"
        line <- getLine
        let parsed = reads line :: [(Int, String)] in
          if length parsed == 0
@@ -24,17 +28,19 @@ getXY board =	do
          else let (x, _) = head parsed in
            if x >= 0 
            then do
-       			putStrLn "Enter a positive y value?"
+       			putStrLn "Enter a positive y value"
        			line <- getLine
        			let parsed = reads line :: [(Int, String)] in
          		  if length parsed == 0
          		  then getX' board
          		  else let (y, _) = head parsed in
            		  	if y >= 0 
-           		  	then print $ hitBoard x y board
+           		  	then (boardToStr sqToStr (hitBoard x y board))
            		  	else getX' board
            else getX' board
        where
          getX' board = do
            putStrLn "Invalid input!"
            getXY board
+
+main = getXY (placeShips [5,4,3,2,2] (mkBoard 10))
